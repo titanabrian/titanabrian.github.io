f328e00a1faff726e0e7d1b428988d2430b46244
Titan Abriansyah <titanabrian@MacBook-Pro.local>
2020-10-01 03:35:21 +0700

    Updates

---
+++ precache-manifest.51a64fbfb20396d2a6334e81b993c00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271cf1ff2f02454dd1cf1da1eb869db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa9a3cfc382e98a1d1e4",
+    "url": "/static/css/main.3dc0210c.chunk.css"
+  },
+  {
+    "revision": "3a05731a0f7228323e61",
+    "url": "/static/js/2.478178db.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.478178db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9a3cfc382e98a1d1e4",
+    "url": "/static/js/main.cf0e1b7e.chunk.js"
+  },
+  {
+    "revision": "e5e1511343bce8242ab1",
+    "url": "/static/js/runtime-main.05f7ccd4.js"
+  }
+]);
